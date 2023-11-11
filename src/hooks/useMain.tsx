11e5303d6f6fd6b86b095ee3c065d92f29d63f1e
halilibrahimcelik <halil.celik139@gmail.com>
2023-11-11 11:11:25 +0300

    feat:added new API route for the prediction

--- src/hooks/useMain.tsx
+++ src/hooks/useMain.tsx
@@ -4,9 +4,12 @@ import { createContext, useContext, useMemo, useState } from "react";
 const initialState = {
   prompt: "",
   onClick: (query: string) => {},
+  error: "",
   generateImage: () => {},
+  prediction: "",
 };
 export const MainContext = createContext(initialState);
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 export const useMainContext = () => {
   const context = useContext(MainContext);
@@ -20,26 +23,63 @@ type Props = {
 };
 export const MainProvider = ({ children }: Props) => {
   const [prompt, setPrompt] = useState("");
+  const [error, setError] = useState("");
+  const [prediction, setPrediction] = useState("");
 
   const onClick = (query: string) => {
     setPrompt(query);
   };
   const generateImage = async () => {
+    // try {
+    //   await fetch("/api/generate", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ prompt }),
+    //   });
+    // } catch (error) {
+    //   console.log(error);
+    //   throw new Error("Something went wrong failed to generate image");
+    // }
+
     try {
-      await fetch("/api/generate", {
+      const response = await fetch("/api/predictions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ prompt }),
+        body: JSON.stringify({
+          prompt,
+        }),
       });
+      let prediction = await response.json();
+
+      if (response.status !== 201) {
+        setError(prediction.detail);
+      }
+
+      while (
+        prediction.status !== "succeeded" &&
+        prediction.status !== "failed"
+      ) {
+        await sleep(1000);
+        const response = await fetch("/api/predictions/" + prediction.id);
+        prediction = await response.json();
+        if (response.status !== 200) {
+          setError(prediction.detail);
+          return;
+        }
+        console.log({ prediction });
+        setPrediction(prediction);
+      }
     } catch (error) {
-      console.log(error);
       throw new Error("Something went wrong failed to generate image");
     }
   };
   const contextValue = useMemo(
-    () => ({ prompt, onClick, generateImage }),
+    () => ({ prompt, onClick, generateImage, error, prediction }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [prompt]
   );
   return (
