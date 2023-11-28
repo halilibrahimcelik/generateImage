972d66dda6aaf77a9a51c84e9a86fd7bfcb67777
halilibrahimcelik <halil.celik139@gmail.com>
2023-11-28 23:01:52 +0300

    feat: bind register page

--- src/containers/loginContainer/index.tsx
+++ src/containers/loginContainer/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Wrapper from "@/components/wrapper";
 import LoginForm from "@/components/loginForm";
+import Link from "next/link";
 type Props = {};
 
 const LoginContainer = (props: Props) => {
   return (
-    <Wrapper tag="section">
+    <Wrapper tag="section" customClass="min-h-[80vh]">
       <LoginForm />
+      <div className="flex flex-col gap-4">
+        <p>Don&apos;t you have an account? </p>
+        <Link className="btn-primary px-6  m-0 py-1 w-fit" href="/register">
+          Sign up
+        </Link>
+      </div>
     </Wrapper>
   );
 };
