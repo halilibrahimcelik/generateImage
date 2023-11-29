d0f7f92bfe251e43ef93375fdaa8de05a6abcd19
halilibrahimcelik <halil.celik139@gmail.com>
2023-11-29 19:34:38 +0300

    feat:route protection added while logged in

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,14 @@
 import RegisterContainer from "@/containers/registerContainer";
 import React from "react";
-
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 type Props = {};
 
-function RegisterPage({}: Props) {
+async function RegisterPage({}: Props) {
+  const session = await getServerSession();
+  if (session) {
+    redirect("/");
+  }
   return <RegisterContainer />;
 }
 
