7d4995d78bd31ee98fe9a8274590bde0cc83a8d2
halilibrahimcelik <halil.celik139@gmail.com>
2023-12-01 18:18:55 +0300

    feat:combined emailjs  with the app

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -1,21 +1,57 @@
 "use client";
 import React from "react";
-
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import { toastConfig } from "@/lib/utils";
 type Props = {};
 
 const ContactForm = (props: Props) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    emailjs
+      .sendForm(
+        `${process.env.NEXT_PUBLIC_GMAIL_SERVICE_ID}`,
+        `${process.env.NEXT_PUBLIC_GMAIL_TEMPLATE_ID}`,
+        e.currentTarget,
+        `${process.env.NEXT_PUBLIC_GMAIL_PUBLIC_KEY}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            toast("Your message has been sent successfully. Thank you!", {
+              type: "success",
+              ...toastConfig,
+            });
+          }
+        },
+        (error) => {
+          console.log(error.text);
+          toast("There has been an error occured,please try again ", {
+            type: "error",
+            ...toastConfig,
+          });
+        }
+      );
+  };
   return (
-    <form className="flex flex-col mx-auto w-full md:w-[80%] lg:w-[40rem] gradient-box ">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col mx-auto w-full md:w-[80%] lg:w-[40rem] gradient-box "
+    >
       <input
         className="input-field "
         type="text"
-        name="name"
+        name="user_name"
         placeholder="Name"
       />
       <input
         className="input-field"
         type="email"
-        name="email"
+        name="user_email"
         placeholder="Email"
       />
       <textarea
